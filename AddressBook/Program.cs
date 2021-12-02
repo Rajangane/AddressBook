3ec0033334c4e8694a59a5d706af83f0788fd14e
Rajangane <pratiksharajangane7@gmail.com>
2021-12-02 12:34:12 +0530

    [Pratiksha] ADD : Created Contacts in Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*************** Address Book *****************");
+//Console.WriteLine("*************** Address Book *****************");
+using AddressBook;
+Console.Write("Select Number:\n1)CreateContacts\n");
+int option = Convert.ToInt32(Console.ReadLine());
+switch (option)
+{
+    case 1:
+        new UC1_CreateContacts().DisplayContacts();
+        break;
+
+    default:
+        Console.Write("Please Select Correct Number");
+        break;
+}
---
+++ AddressBook/UC1_CreateContacts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class UC1_CreateContacts
+    {
+        public string firstname;
+        public string lastname;
+        public string address;
+        public string city;
+        public string state;
+        public string zipcode;
+        public string phonenumber;
+        public string email;
+
+        public  void DisplayContacts()
+        {
+            UC1_CreateContacts cc = new UC1_CreateContacts();
+
+            Console.Write("Enter First Name: ");
+            cc.firstname = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            cc.lastname = Console.ReadLine();
+
+            Console.Write("Enter the Address: ");
+            cc.address = Console.ReadLine();
+
+            Console.Write("Enter City name: ");
+            cc.city = Console.ReadLine();
+
+            Console.Write("Enter State name: ");
+            cc.state = Console.ReadLine();
+
+            Console.Write("Enter the Zip Code: ");
+            cc.zipcode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            cc.phonenumber = Console.ReadLine();
+
+            Console.Write("Enter the Email ID: ");
+            cc.email = Console.ReadLine();
+
+
+            Console.WriteLine("\nDetails of the Person are given below: ");
+            Console.WriteLine("\nFirst Name: " + cc.firstname);
+            Console.WriteLine("\nLast Name: " + cc.lastname);
+            Console.WriteLine("\nAddress: " + cc.address);
+            Console.WriteLine("\nCity: " + cc.city);
+            Console.WriteLine("\nState: " + cc.state);
+            Console.WriteLine("\nZip Code: " + cc.zipcode);
+            Console.WriteLine("\nPhone Number: " + cc.phonenumber);
+            Console.WriteLine("\nEmail ID: " + cc.email);
+            Console.WriteLine("-------------------------------------------");
+
+        }
+    }
+}
